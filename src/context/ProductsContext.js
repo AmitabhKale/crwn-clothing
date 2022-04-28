297cd8c5154cc7069c855e47cefbc8cb28de1b46
Amitabh Kale <amitabhkale@gmail.com>
2022-04-28 12:25:59 +0530

    Add Categories to Firebase

--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,13 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { addCollectionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase/Firebase.js";
 
-import PRODUCTS from '../shop-data.json';
+// import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
     products: []
 })
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+        useEffect(() => {
+           const getCategoriesMap = async () => {
+                const categoryMap = await getCategoriesAndDocuments();
+                console.log(categoryMap);
+           }
+           getCategoriesMap();
+        },[])
+
     const value = { products };
     return (
         <ProductsContext.Provider value={value}>{children} </ProductsContext.Provider>
